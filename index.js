9e38d59a82ddd70f994216b32426ab86011183c8
Agus Salim <agokz52@gmail.com>
2026-01-01 10:38:30 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById('satu').onclick = () => {
+    document.getElementById('ctn2').scrollIntoView({ behavior: 'smooth' });
+};
+document.getElementById('dua').onclick = () => {
+    document.getElementById('ctn3').scrollIntoView({ behavior: 'smooth' });
+};
